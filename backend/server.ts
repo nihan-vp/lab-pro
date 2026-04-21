2426da006523c31a98fea68bb1ccfba67f023f2e
nihan-vp <qwerty311980@gmail.com>
2026-04-21 10:35:01 +0530

    feat: implemented pending reqests and Recent Activities in dashboard.

--- backend/server.ts
+++ backend/server.ts
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename);
 
 const JWT_SECRET = process.env.JWT_SECRET || 'f89c0e295327ba553a5a0400545b0400f6d5a69d4490b01ca7c2b444008a078999f81e1a5fe359c77fc06ed868bca2e6af7ae46a66c94cb0af8ba6bb7a9fe7a1';
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://qwerty311980_db_user:8XggP8d16ZsHeffh@binformed.lfjrbxn.mongodb.net/lab-ms';
-
+const VITE_API_URL = process.env.VITE_API_URL || 'https://lab-pro-2qns.onrender.com/';
 async function connectDB() {
   if (mongoose.connection.readyState >= 1) return;
   try {
